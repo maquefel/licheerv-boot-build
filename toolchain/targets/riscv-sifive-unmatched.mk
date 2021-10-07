ee8f4c188dfe4cb2b29d793a5c0338276c45d653
Nikita Shubin <n.shubin@yadro.com>
2021-10-07 17:18:20 +0300

    target: fix float abi
    Signed-off-by: Nikita Shubin <n.shubin@yadro.com>

--- toolchain/targets/riscv-sifive-unmatched.mk
+++ toolchain/targets/riscv-sifive-unmatched.mk
@@ -5,6 +5,7 @@ TARGET_ARCH_AUX ?= riscv64
 TARGET_ARCH_AUX2 ?= rv64gc
 TARGET_CPU ?= sifive-7-series
 TARGET_ARCH_EXTRA_FLAGS ?= -march=${TARGET_ARCH_AUX2} -mtune=${TARGET_CPU}
+TARGET_FLOAT_ABI ?= lp64d
 # riscv64-unknown-linux-gnu
 TARGET_OS ?= ${TARGET_ARCH_AUX}-unknown-linux-gnu
 TARGET_CROSS ?= ${TARGET_OS}
